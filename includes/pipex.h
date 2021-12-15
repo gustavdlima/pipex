bbca03cec9c2849591b0d7bf414722545b606db6
gusalves <gusalves@student.42sp.org.br>
2021-12-15 19:27:51 +0100

    START

---
+++ includes/pipex.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gusalves <gusalves@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/15 18:57:50 by gusalves          #+#    #+#             */
+/*   Updated: 2021/12/15 19:18:25 by gusalves         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+typedef struct s_pipex {
+
+}				t_pipex;
+
+// msg functions
+void	msg_out(char *string, t_pipex pipex);
+
+#endif
